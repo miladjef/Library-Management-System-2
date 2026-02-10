210eecc02fa972709126a98b2acc5ca13930848d
milad jafari <miladjef@yahoo.com>
2026-02-10 14:51:20 +0330

    fix add_reservation.php

--- admin/add_reservation.php
+++ admin/add_reservation.php
@@ -1,4 +1,11 @@
 <?php
+require_once 'inc/functions.php';
+
+if (!is_logged_in() || !is_admin($_SESSION['userid'])) {
+    header('Location: login.php');
+    exit;
+}
+
 $title = "امانت دادن دستی کتاب";
 include "inc/header.php"; ?>
 <?php include "inc/add_reservation.php"; ?>
