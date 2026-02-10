091e0f2b84fffd7ea8e35415d3b6ce3df2fea885
milad jafari <miladjef@yahoo.com>
2026-02-10 15:27:17 +0330

    Fix file

--- inc/search_box.php
+++ inc/search_box.php
@@ -4,9 +4,10 @@ require_once __DIR__ . '/includes/security.php';
 <div class="search-area">
     <form action="search.php" method="GET">
         <input id="search" name="search"
-               placeholder="نام کتاب مورد نظر خود را وارد کنید ..." type="text">
-        <button type="submit"> جستجوی کتاب</button>
+               placeholder="نام کتاب مورد نظر خود را وارد کنید ..."
+               type="text"
+               value="<?php echo htmlspecialchars($_GET['search'] ?? '', ENT_QUOTES, 'UTF-8'); ?>"
+               maxlength="100">
+        <button type="submit">جستجوی کتاب</button>
     </form>
 </div>
-
-
